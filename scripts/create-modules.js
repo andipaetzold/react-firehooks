235029ef741070d834f16e846a0793e7939148ce
Andi Pätzold <github@andipaetzold.com>
2021-10-15 13:16:14 +0200

    fix: mark modules as side-effect-free

--- scripts/create-modules.js
+++ scripts/create-modules.js
@@ -6,6 +6,7 @@ for (const module of modules) {
         main: `../lib/cjs/${module}/index.js`,
         module: `../lib/esm/${module}/index.js`,
         types: `../lib/types/${module}/index.d.ts`,
+        sideEffects: false,
     };
 
     if (!fs.existsSync(module)) {
