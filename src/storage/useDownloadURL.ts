359e98a9dba3f00890bd2587919fef6221a0abda
Andi Pätzold <github@andipaetzold.com>
2021-10-16 01:29:00 +0200

    docs: add jsdocs for `useDownloadURL`

--- src/storage/useDownloadURL.ts
+++ src/storage/useDownloadURL.ts
@@ -11,7 +11,10 @@ export type UseDownloadURLResult = ValueHookResult<string, StorageError>;
  * Returns the download URL of a Google Cloud Storage object
  *
  * @param {StorageReference | undefined | null} reference Reference to a Google Cloud Storage object
- * @returns {UseDownloadURLResult}
+ * @returns {UseDownloadURLResult} Download URL, loading state, and error
+ * * value: Download URL; `undefined` if download URL is currently being fetched, or an error occurred
+ * * loading: `true` while fetching the download URL; `false` if the download URL was fetched successfully or an error occurred
+ * * error: `undefined` if no error occurred
  */
 export function useDownloadURL(reference: StorageReference | undefined | null): UseDownloadURLResult {
     const isMounted = useIsMounted();
