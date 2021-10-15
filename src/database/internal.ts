a096c41223a45c096172a7e7b60fd4e11938aaa4
Andi Pätzold <github@andipaetzold.com>
2021-10-15 22:06:23 +0200

    fix(database): use new query when changing from/to undefined

--- src/database/internal.ts
+++ src/database/internal.ts
@@ -5,7 +5,7 @@ import { useStableValue } from "../util/useStableValue";
  * @internal
  */
 function isQueryEqual(a: Query | undefined, b: Query | undefined): boolean {
-    const areBothUndefined = a === undefined || b === undefined;
+    const areBothUndefined = a === undefined && b === undefined;
     const areSameRef = a !== undefined && b !== undefined && a.isEqual(b);
     return areBothUndefined || areSameRef;
 }
