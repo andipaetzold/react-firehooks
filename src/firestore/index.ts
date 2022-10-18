b04a692094676b3f99bdbc303d807b71946f067c
Andi Pätzold <github@andipaetzold.com>
2022-10-18 20:56:29 +0200

    feat(firestore): add useCountFromServer (#147)

--- src/firestore/index.ts
+++ src/firestore/index.ts
@@ -3,6 +3,7 @@ export * from "./useCollection.js";
 export * from "./useCollectionData.js";
 export * from "./useCollectionDataOnce.js";
 export * from "./useCollectionOnce.js";
+export * from "./useCountFromServer.js";
 export * from "./useDocument.js";
 export * from "./useDocumentData.js";
 export * from "./useDocumentDataOnce.js";
